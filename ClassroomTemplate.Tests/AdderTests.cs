868b701a65e2031fd6ca45ab798ad1049443d3f0
MrMartinezCTC <106337782+MrMartinezCTC@users.noreply.github.com>
2023-01-25 13:11:16 -0500

    Update AdderTests.cs

--- ClassroomTemplate.Tests/AdderTests.cs
+++ ClassroomTemplate.Tests/AdderTests.cs
@@ -1,18 +1,16 @@
-﻿using Xunit;
+using Xunit;
 
 namespace ClassroomTemplate.Tests;
 
 public class AdderTests
 {
     [Theory]
-    [InlineData(2, 2, 4)]
-    [InlineData(5, 0, 5)]
     [InlineData(0, 5, 5)]
+    [InlineData(2, 3, 5)]
+    [InlineData(5, 0, 5)]
     [Trait("TestGroup", "Adder")]
     public void Adder_Sum_GivesSumOfParameters(int first, int second, int expected)
-    {
-        var adder = new Adder(first, second);
-
-        Assert.Equal(expected, adder.Sum);
+    {        
+        Assert.Equal(expected, Adder.Sum(first, second));
     }
 }
